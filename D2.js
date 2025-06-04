1a53e960f75306f35af5489ca18d67b33b20c954
PernaGiuseppe <giuseppeperna97@hotmail.com>
2025-06-04 17:07:16 +0200

    exercise 6 not understood, the others complete

--- D2.js
+++ D2.js
@@ -192,12 +192,12 @@ console.log(me)
 /* ESERCIZIO 14
   Scrivi del codice per creare un array inizialmente vuoto. Riempilo successivamente con i numeri da 1 a 10.
 */
-
-let array = []
-let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+let array = null
+array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 /* ESERCIZIO 15
   Scrivi del codice per sostituire l'ultimo elemento dell'array, ovvero il valore 10, con il valore 100.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
+array.splice(9, 1, 100)
+console.log(array)
